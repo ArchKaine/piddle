dfeac2806b32beec51fc8eb014a387ab34e2359c
Manuel Debaux <debaux.manuel@gmail.com>
2011-10-02 13:13:31 +0200

    added function getOverallLength to show an accurate download speed for progressbar

--- pydl.py
+++ pydl.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python2
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Filename: pydl.py                                                 #
-# Author: Brian Tomlinson <darthlukan@gmail.com>                     #
+# Authors: Brian Tomlinson <darthlukan@gmail.com>                   #
+#          Manuel Debaux <debaux.manual@gmail.com>                  #
+#          Brian Turner <archkaine@gmail.com>                       #
 # URL: git@github.com:darthlukan/piddle.git                         #
 # Description: A simple CLI download manager written in Python.     #
 # Warning: If you received this program from any source other than  #
@@ -15,41 +17,90 @@
 # os and sys are needed to make sure that files and system level stuff
 # are handled properly.  urllib(2) for communications (we are downloading)
 # fileinput handles looping over links in a file (txt for now, csv later)
-# progressbar adds some bling for the user to look at while we work.
+# progressbar adds some bling for the user to look at while we work.  To get
+# progressbar to work, pip2 install progressbar.
 
 import os
 import sys
 import urllib
 import urllib2
 import fileinput
-from progressbar import ProgressBar
+from progressbar import *
 
 #Introduce ourselves
 print("""Hello! I am going to ensure that downloading your file, renaming it,
 and specifying where to save it, are as simple as possible. Let's get to it!""")
 
 # Warn the user about non-existent feature
-print('Be warned! File Looping has not yet been implemented and will cause an exit.')
-
+print('Be warned! File Looping has been implemented but is experimental.')
+print('Downloading large groups of files could lead to RAM abuse.')
 # The function that actually gets stuff
-# IT WORKS!!!
-def getDownload():  # Grab the file
+def getDownload(urlToGetFile, fileNameToSave):  # Grab the file(s)
     urllib.urlretrieve(urlToGetFile, fileNameToSave)
 
-#Define initial globals,
-specialDownload = raw_input('Do you need to import a file with links?(y/n): ')
-if specialDownload == 'n':
-    urlToGetFile = raw_input('Please enter the download URL: ')
-    fileNameToSave = raw_input('Enter the desired filename: ')
-    getDownload()
-elif specialDownload == 'y':
-    print('This feature has not yet been implemented! Please re-run the program')
-    exit("Feature 'file looping' not yet implemented.")
-else:
-    print('There was a problem with your response, please re-run the program')
-    exit("User input invalid or unreadable.")
+    # Placeholder for progressbar:
+    #widgets = ['Overall Progress: ', Percentage(), ' ',
+    #               Bar(marker='#',left='[',right=']'),
+    #               ' ', ETA(), ' ', FileTransferSpeed()]
+    #pbar = ProgressBar(widgets=widgets, maxval=nl)
+    #pbar.start()
 
-# Call our main function
-getDownload()
+# The function that sums the lengths of all files to download
+# This function avoid to download all files to get lengths but it's take quite time to get few files length
+def getOverallLength(fileNameUrls):
+    fi = fileinput.input(fileNameUrls)
+    overallLength=0
+    
+    for line in fi:
+        data=str(urllib2.urlopen(line[:-1]).info())
+        data=data[data.find("Content-Length"):]
+        data=data[16:data.find("\r")]
+        overallLength+=int(data)
+        
+    return overallLength
 
-
+def fileLoopCheck():
+    specialDownload = raw_input('Do you need to import a file with links?(y/n): ')
+    if specialDownload == 'n':
+        urlToGetFile = raw_input('Please enter the download URL: ')
+        fileNameToSave = raw_input('Enter the desired filename: ')
+        getDownload()
+    elif specialDownload == 'y':
+        fileNameUrls = raw_input('Enter the filename (with path) that contains URLs: ')
+        baseDir = raw_input('Enter the directory where to download files: ')
+        
+        overallLength=getOverallLength(fileNameUrls) # getting overall length files size
+        
+        # Define how to handle pathing, default to preceding '/'
+        if not baseDir.endswith("/") and baseDir != "":
+            baseDir+="/"
+        # Grab the file and iterate over each line, this is not yet smart enough
+        # to discern between an actual url and erroneous text, so don't have anything
+        # other than links in your input file!
+        fi = fileinput.input(fileNameUrls)
+        nl=0 #numbers of line
+        for line in fi:
+            nl+=1 # iterate over the next line
+        # Re-read, this will be cleaned up later
+        fi = fileinput.input(fileNameUrls) # reset the fileinput : can't reuse it
+        cl=0 # currentline
+        # Progressbar() stuff, wheee!
+        widgets = ['Overall Progress: ', Percentage(), ' ',
+                       Bar(marker='>',left='[',right=']'),
+                       ' ', ETA(), ' ', FileTransferSpeed()]
+        pbar = ProgressBar(widgets=widgets, maxval=overallLength)
+        pbar.start()
+        # Done with the prep work, time to do what the user wants
+        for line in fi:
+            urlToGetFile=line[:-1]
+            fileNameToSave=baseDir+urlToGetFile[urlToGetFile.rfind('/')+1:]
+            getDownload(urlToGetFile, fileNameToSave)
+            cl+=1
+            pbar.update(overallLength/nl*cl)
+        pbar.finish()
+        print('All done!')
+    else:
+        print('There was an error in your response, let\'s try again...')
+        fileLoopCheck()
+# Call start function
+fileLoopCheck()
